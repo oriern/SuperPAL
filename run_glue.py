4979026619aed8d7ca77d5de6ea8b38b613e6398
oriern <oriern@gmail.com>
2023-03-01 21:47:29 +0200

    Update run_glue.py

--- run_glue.py
+++ run_glue.py
@@ -63,7 +63,7 @@ from transformers import glue_compute_metrics as compute_metrics
 from transformers import glue_convert_examples_to_features as convert_examples_to_features
 from transformers import glue_output_modes as output_modes
 from transformers import glue_processors as processors
-from transformers import finalAlignmentPred
+import finalAlignmentPred
 
 
 from transformers.modeling_roberta import RobertaClassificationHead
